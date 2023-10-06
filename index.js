a1b86838b34c54d6d2a1582e07846cf24d2071b2
federico <apache1280h@gmail.com>
2023-10-06 15:53:00 -0500

    Agregamos variables de entorno

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const dotenv = require("dotenv")
+//dotenv.config()
+
+//const dotenv = {
+//    config : function(){
+//        console.log("Metodo del objeto dot env")
+//    }
+//}
+
+//dotenv.config()
 
 const http = require("http")
 
 //const exportsfromanother = require("./another")
 //console.log({exportsfromanother})
-
-
 
 function requirecontroller(){
     console.log("Aqui hay un nuevo pedido, que pasa, esto es nuevo")
@@ -12,7 +20,14 @@ function requirecontroller(){
 
 const server = http.createServer(requirecontroller)
 
-server.listen(4000)
+const PORT = process.env.PORT
+
+
+
+server.listen(4000,function(){
+//    console.log({env: process.env})
+console.log("Aplicacion corriendo en el puerto: " + PORT)
+})
 
 
 
